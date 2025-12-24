8e819837674bd2fa4710e2f4e0146da5134cae0a
Ketan Gupta <ketankumarnna@gmail.com>
2025-12-24 19:22:30 +0530

    zero-shot-prompting added

---
+++ prompts/zero_shot_prompting.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key = os.getenv('GEMINI_API_KEY') ,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+SYSTEM_PROMPT = "Give answer only in python. And do not give any answer other tham coding, just say Sorry! I am designed to answer coding related questions."
+
+
+# Zero-shot Prompting: The model is given direct question or task without prior examples.
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": "Can you say a joke?"}
+    ]
+)
+
+print(response.choices[0].message.content)
